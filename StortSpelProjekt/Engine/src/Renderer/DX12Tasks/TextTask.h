fda1190a08eccb2ed0ba8ab8e9a2581ceb5a98c8
Fredrik Lind <f.flind@hotmail.com>
2020-09-08 14:40:15 +0200

    Fonts folder, relevant classes and shaders are added
    A new resource folder has been added called Fonts. This folder is supposed to host all the different fonts we want to use in the game. 
    A new class called Text has been added to Renderer, where some necessary structs have been declared. 
    A TextTask class and two new shaders called TextVertex and TextPixel have also been added.

---
+++ StortSpelProjekt/Engine/src/Renderer/DX12Tasks/TextTask.h
@@ -0,0 +1,24 @@
+#ifndef TEXTTASK_H
+#define TEXTTASK_H
+
+#include "RenderTask.h"
+
+class RootSignature;
+
+class TextTask : public RenderTask
+{
+public:
+	TextTask(ID3D12Device5* device,
+		RootSignature* rootSignature,
+		LPCWSTR VSName, LPCWSTR PSName,
+		std::vector<D3D12_GRAPHICS_PIPELINE_STATE_DESC*>* gpsds,
+		LPCTSTR psoName);
+	~TextTask();
+
+	void Execute();
+
+private:
+
+};
+
+#endif
