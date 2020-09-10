575c77fa379e687f70004f50a8eca7c21068957a
Anton Å <anton.asbrink@hotmail.com>
2020-09-10 07:25:32 +0200

    Added connection and tested
    Only tested with localhost. Cannot guarantee successful connection over internet.

--- StortSpelProjekt/Engine/src/Network/Network.cpp
+++ StortSpelProjekt/Engine/src/Network/Network.cpp
@@ -9,14 +9,15 @@ bool Network::ConnectToIP(std::string ip, int port)
 {
     sf::Socket::Status status = socket.connect(ip, port);
 
-    //if (status != sf::Socket::Done()) {
-        Log::PrintSeverity(Log::Severity::WARNING, "Connection to " + ip + " failed");
+    if (status != 0) 
+    {
+        Log::PrintSeverity(Log::Severity::WARNING, "Connection to " + ip + " failed\n");
         return false;
-    //}
-    //else {
-        Log::Print("Connected to " + ip);
+    }
+    else {
+        Log::Print("Connected to " + ip + "\n");
         return true;
-    //}
+    }
 }
 
 void Network::ListenConnection(int port)
@@ -24,11 +25,13 @@ void Network::ListenConnection(int port)
     // bind the listener to a port
     if (listener.listen(port) != sf::Socket::Done)
     {
-        // error...
+        Log::PrintSeverity(Log::Severity::WARNING, "Failed attempting to listen to port: " + std::to_string(port) + " failed\n");
     }
 
     if (listener.accept(socket) != sf::Socket::Done)
     {
-        // error...
+        Log::PrintSeverity(Log::Severity::WARNING, "Failed connection to " + socket.getRemoteAddress().toString() + "\n");
     }
+
+    Log::Print("Connected to " + socket.getRemoteAddress().toString() + "\n");
 }
--- StortSpelProjekt/Server/src/main.cpp
+++ StortSpelProjekt/Server/src/main.cpp
@@ -5,5 +5,21 @@ int main() {
 
 	Network network;
 
+	std::string ip;
+
+	std::cin >> ip;
+
+	if (ip == "1") {
+		std::cin >> ip;
+		std::cout << "Connecting to " + ip << std::endl;
+		network.ConnectToIP(ip, 55555);
+	}
+	else {
+		std::cout << "Listening for connections" << std::endl;
+		network.ListenConnection(55555);
+	}
+
+	std::getchar();
+
 	return 0;
 }
