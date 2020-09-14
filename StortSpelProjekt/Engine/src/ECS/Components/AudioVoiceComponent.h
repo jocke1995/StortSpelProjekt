521aa5dc288caad449987e37dc74ca1b0b37c7c2
Andres <kattalainen@hotmail.com>
2020-09-14 16:17:01 +0200

    Added some comments

--- StortSpelProjekt/Engine/src/ECS/Components/AudioVoiceComponent.h
+++ StortSpelProjekt/Engine/src/ECS/Components/AudioVoiceComponent.h
@@ -16,9 +16,13 @@ namespace component
 		virtual ~AudioVoiceComponent();
 		void Update(double dt);
 
+		// Clones an audiobuffer to create a voice to the component
 		void AddVoice(const std::wstring& name);
 
+		// Playback options of audio
+		// Play audio
 		void Play(const std::wstring& name);
+		// Stop audio
 		void Stop(const std::wstring& name);
 
 	private:
