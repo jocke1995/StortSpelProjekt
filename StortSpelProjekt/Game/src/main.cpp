405141a4629aca00cc11b9edaa0dba7286b05412
Tim Johansson <totimom@live.se>
2020-09-22 15:25:56 +0200

    Moved playerinputcomponent to separate folder

--- StortSpelProjekt/Game/src/main.cpp
+++ StortSpelProjekt/Game/src/main.cpp
@@ -1,5 +1,5 @@
 #include "Engine.h"
-#include "PlayerInputComponent.h"
+#include "Components/PlayerInputComponent.h"
 Scene* GetDemoScene(SceneManager* sm);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
