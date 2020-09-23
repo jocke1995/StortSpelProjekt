791018bcb683f76a9ceb5e2f3fd25ec068b11d49
Leo Wikström <wikstrom.hl@gmail.com>
2020-09-23 10:42:01 +0200

    Changed some stuff

--- StortSpelProjekt/Engine/src/Renderer/DX12Tasks/ImGuiRenderTask.cpp
+++ StortSpelProjekt/Engine/src/Renderer/DX12Tasks/ImGuiRenderTask.cpp
@@ -38,8 +38,29 @@ void ImGuiRenderTask::Execute()
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 
-	bool show_demo_window = false;
-	ImGui::ShowDemoWindow(&show_demo_window);
+	float* clear_color;
+
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+		{
+			counter++;
+		}
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
 
 	// Record commandlist
 	ID3D12CommandAllocator* commandAllocator = m_pCommandInterface->GetCommandAllocator(m_CommandInterfaceIndex);
