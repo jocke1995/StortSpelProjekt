6a919a88132c398c7cdc33ed82cff892122ce34a
Leo Wikström <wikstrom.hl@gmail.com>
2020-09-24 16:55:01 +0200

    Basic console

--- StortSpelProjekt/Engine/src/ImGUI/ImGuiHandler.cpp
+++ StortSpelProjekt/Engine/src/ImGUI/ImGuiHandler.cpp
@@ -1,4 +1,6 @@
 #include "ImGuiHandler.h"
+#include "../Misc/Option.h"
+#include "../Events/EventBus.h"
 
 ImGuiHandler& ImGuiHandler::GetInstance()
 {
@@ -17,29 +19,7 @@ void ImGuiHandler::NewFrame()
 
 void ImGuiHandler::UpdateFrame()
 {
-    static int counter = 0;
-
-    ImGui::Begin("Hello there!");                          // Create a window called "Hello, world!" and append into it.
-    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-
-    if (m_FloatMap.find("LightPositionZ") != m_FloatMap.end())
-    {
-        ImGui::SliderFloat("float", &m_FloatMap["LightPositionZ"], -15.0f, 15.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-    }
-    if (m_Vec4Map.find("LightColor") != m_Vec4Map.end())
-    {
-        ImGui::ColorEdit3("clear color", reinterpret_cast<float*>(&m_Vec4Map["LightColor"])); // Edit 3 floats representing a color
-    }
-
-    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-    {
-        counter++;
-    }
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
-
-    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    ImGui::End();
+    DrawConsole("Console");
 }
 
 void ImGuiHandler::SetFloat(std::string name, float value)
@@ -114,10 +94,345 @@ DirectX::XMFLOAT4 ImGuiHandler::GetXMFLOAT4(std::string name)
     }
 }
 
+void ImGuiHandler::ClearLog()
+{
+    for (int i = 0; i < m_Items.Size; ++i)
+    {
+        free(m_Items[i]);
+    }
+    m_Items.clear();
+}
+
+void ImGuiHandler::AddLog(const char* fmt, ...) IM_FMTARGS(2)
+{
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buf, IM_ARRAYSIZE(buf), fmt, args);
+    buf[IM_ARRAYSIZE(buf) - 1] = 0;
+    va_end(args);
+    m_Items.push_back(Strdup(buf));
+}
+
+void ImGuiHandler::DrawConsole(const char* title)
+{
+    ImGui::SetNextWindowSize(ImVec2(Option::GetInstance().GetVariable("windowWidth") / 2, Option::GetInstance().GetVariable("windowHeight")));
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowCollapsed(true, ImGuiCond_FirstUseEver);
+    if (!ImGui::Begin(title))
+    {
+        ImGui::End();
+        return;
+    }
+
+    ImGui::TextWrapped("Enter 'HELP' for help.");
+
+    if (ImGui::SmallButton("Clear"))
+    {
+        ClearLog();
+    }
+
+    ImGui::Separator();
+
+    m_Filter.Draw("Filter (\"incl,-excl\")", 180);
+    ImGui::Separator();
+
+    // Reserve enough left-over height for 1 separator + 1 input text
+    const float footer_height_to_reserve = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
+    ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height_to_reserve), false, ImGuiWindowFlags_HorizontalScrollbar);
+    if (ImGui::BeginPopupContextWindow())
+    {
+        if (ImGui::Selectable("Clear"))
+        {
+            ClearLog();
+        }
+        ImGui::EndPopup();
+    }
+
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1)); // Tighten spacing
+    for (int i = 0; i < m_Items.Size; ++i)
+    {
+        const char* item = m_Items[i];
+        if (!m_Filter.PassFilter(item))
+        {
+            continue;
+        }
+
+        // Normally you would store more information in your item than just a string.
+        // (e.g. make Items[] an array of structure, store color/type etc.)
+        ImVec4 color;
+        bool has_color = false;
+        if (strstr(item, "[error]"))
+        {
+            color = ImVec4(1.0f, 0.4f, 0.4f, 1.0f); has_color = true;
+        }
+        else if (strncmp(item, "# ", 2) == 0)
+        {
+            color = ImVec4(1.0f, 0.8f, 0.6f, 1.0f); has_color = true;
+        }
+
+        if (has_color)
+        {
+            ImGui::PushStyleColor(ImGuiCol_Text, color);
+        }
+        ImGui::TextUnformatted(item);
+        if (has_color)
+        {
+            ImGui::PopStyleColor();
+        }
+    }
+
+    if (m_ScrollToBottom)
+    {
+        ImGui::SetScrollHereY(1.0f);
+    }
+    m_ScrollToBottom = false;
+
+    ImGui::PopStyleVar();
+    ImGui::EndChild();
+    ImGui::Separator();
+
+    // Command-line
+    bool reclaim_focus = false;
+    ImGuiInputTextFlags input_text_flags = ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackCompletion | ImGuiInputTextFlags_CallbackHistory;
+    if (ImGui::InputText("Input", m_InputBuf, IM_ARRAYSIZE(m_InputBuf), input_text_flags, &TextEditCallbackStub, static_cast<void*>(this)))
+    {
+        char* s = m_InputBuf;
+        Strtrim(s);
+        if (s[0])
+        {
+            ExecCommand(s);
+        }
+        strcpy(s, "");
+        reclaim_focus = true;
+    }
+
+    // Auto-focus on window apparition
+    ImGui::SetItemDefaultFocus();
+    if (reclaim_focus)
+    {
+        ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
+    }
+
+    ImGui::End();
+}
+
+void ImGuiHandler::ExecCommand(const char* command_line)
+{
+    AddLog("# %s\n", command_line);
+
+    // Insert into history. First find match and delete it so it can be pushed to the back.
+    // This isn't trying to be smart or optimal.
+    m_HistoryPos = -1;
+    for (int i = m_History.Size - 1; i >= 0; --i)
+        if (Stricmp(m_History[i], command_line) == 0)
+        {
+            free(m_History[i]);
+            m_History.erase(m_History.begin() + i);
+            break;
+        }
+    m_History.push_back(Strdup(command_line));
+
+    char command[256];
+    for (int i = 0; i < 256; ++i)
+    {
+        if (!command_line[i])
+        {
+            break;
+        }
+        else if (command_line[i] == ':')
+        {
+
+        }
+        command[i] = command_line[i];
+    }
+
+    // Process command
+    if (Stricmp(command_line, "CLEAR") == 0)
+    {
+        ClearLog();
+    }
+    else if (Stricmp(command_line, "HELP") == 0)
+    {
+        AddLog("Commands:");
+        for (int i = 0; i < m_Commands.Size; ++i)
+        {
+            AddLog("- %s", m_Commands[i]);
+        }
+    }
+    else if (Stricmp(command_line, "HISTORY") == 0)
+    {
+        int first = m_History.Size - 10;
+        for (int i = first > 0 ? first : 0; i < m_History.Size; ++i)
+        {
+            AddLog("%3d: %s\n", i, m_History[i]);
+        }
+    }
+    else if (Stricmp(command_line, "UNLOCK CAMERA") == 0)
+    {
+        EventBus::GetInstance().Publish(&ModifierInput(SCAN_CODES::LEFT_CTRL, true));
+    }
+    else
+    {
+        AddLog("Unknown command: '%s'\n", command_line);
+    }
+
+    // On command input, we scroll to bottom even if AutoScroll==false
+    m_ScrollToBottom = true;
+}
+
+int ImGuiHandler::TextEditCallbackStub(ImGuiInputTextCallbackData* data)
+{
+    ImGuiHandler* console = reinterpret_cast<ImGuiHandler*>(data->UserData);
+    return console->TextEditCallback(data);
+}
+
+int ImGuiHandler::TextEditCallback(ImGuiInputTextCallbackData* data)
+{
+    //AddLog("cursor: %d, selection: %d-%d", data->CursorPos, data->SelectionStart, data->SelectionEnd);
+    switch (data->EventFlag)
+    {
+    case ImGuiInputTextFlags_CallbackCompletion:
+        {
+            // Example of TEXT COMPLETION
+
+            // Locate beginning of current word
+            const char* word_end = data->Buf + data->CursorPos;
+            const char* word_start = word_end;
+            while (word_start > data->Buf)
+            {
+                const char c = word_start[-1];
+                if (c == ' ' || c == '\t' || c == ',' || c == ';')
+                {
+                    break;
+                }
+                --word_start;
+            }
+
+            // Build a list of candidates
+            ImVector<const char*> candidates;
+            for (int i = 0; i < m_Commands.Size; ++i)
+            {
+                if (Strnicmp(m_Commands[i], word_start, static_cast<int>(word_end - word_start)) == 0)
+                {
+                    candidates.push_back(m_Commands[i]);
+                }
+            }
+
+            if (candidates.Size == 0)
+            {
+                // No match
+                AddLog("No match for \"%.*s\"!\n", static_cast<int>(word_end - word_start), word_start);
+            }
+            else if (candidates.Size == 1)
+            {
+                // Single match. Delete the beginning of the word and replace it entirely so we've got nice casing.
+                data->DeleteChars((int)(word_start - data->Buf), static_cast<int>(word_end - word_start));
+                data->InsertChars(data->CursorPos, candidates[0]);
+                data->InsertChars(data->CursorPos, " ");
+            }
+            else
+            {
+                // Multiple matches. Complete as much as we can..
+                // So inputing "C"+Tab will complete to "CL" then display "CLEAR" and "CLASSIFY" as matches.
+                int match_len = static_cast<int>(word_end - word_start);
+                for (;;)
+                {
+                    int c = 0;
+                    bool all_candidates_matches = true;
+                    for (int i = 0; i < candidates.Size && all_candidates_matches; ++i)
+                    {
+                        if (i == 0)
+                        {
+                            c = toupper(candidates[i][match_len]);
+                        }
+                        else if (c == 0 || c != toupper(candidates[i][match_len]))
+                        {
+                            all_candidates_matches = false;
+                        }
+                    }
+                    if (!all_candidates_matches)
+                    {
+                        break;
+                    }
+                    ++match_len;
+                }
+
+                if (match_len > 0)
+                {
+                    data->DeleteChars(static_cast<int>(word_start - data->Buf), static_cast<int>(word_end - word_start));
+                    data->InsertChars(data->CursorPos, candidates[0], candidates[0] + match_len);
+                }
+
+                // List matches
+                AddLog("Possible matches:\n");
+                for (int i = 0; i < candidates.Size; ++i)
+                {
+                    AddLog("- %s\n", candidates[i]);
+                }
+            }
+
+            break;
+        }
+    case ImGuiInputTextFlags_CallbackHistory:
+        {
+            // Example of HISTORY
+            const int prev_history_pos = m_HistoryPos;
+            if (data->EventKey == ImGuiKey_UpArrow)
+            {
+                if (m_HistoryPos == -1)
+                {
+                    m_HistoryPos = m_History.Size - 1;
+                }
+                else if (m_HistoryPos > 0)
+                {
+                    --m_HistoryPos;
+                }
+            }
+            else if (data->EventKey == ImGuiKey_DownArrow)
+            {
+                if (m_HistoryPos != -1)
+                {
+                    if (++m_HistoryPos >= m_History.Size)
+                    {
+                        m_HistoryPos = -1;
+                    }
+                }
+            }
+
+            // A better implementation would preserve the data on the current input line along with cursor position.
+            if (prev_history_pos != m_HistoryPos)
+            {
+                const char* history_str = (m_HistoryPos >= 0) ? m_History[m_HistoryPos] : "";
+                data->DeleteChars(0, data->BufTextLen);
+                data->InsertChars(0, history_str);
+            }
+        }
+    }
+    return 0;
+}
+
 ImGuiHandler::ImGuiHandler()
 {
+    m_TextMap["console"] = " ";
+
+    ClearLog();
+    memset(m_InputBuf, 0, sizeof(m_InputBuf));
+    m_HistoryPos = -1;
+
+    m_Commands.push_back("HELP");
+    m_Commands.push_back("HISTORY");
+    m_Commands.push_back("CLEAR");
+    m_Commands.push_back("UNLOCK CAMERA");
+    m_ScrollToBottom = false;
+    AddLog("Console initiated!");
 }
 
 ImGuiHandler::~ImGuiHandler()
 {
+    ClearLog();
+    for (int i = 0; i < m_History.Size; ++i)
+    {
+        free(m_History[i]);
+    }
 }
--- StortSpelProjekt/Engine/src/ImGUI/ImGuiHandler.h
+++ StortSpelProjekt/Engine/src/ImGUI/ImGuiHandler.h
@@ -23,12 +23,36 @@ public:
 	float4 GetFloat4(std::string name);
 	DirectX::XMFLOAT4 GetXMFLOAT4(std::string name);
 
+	// For console
+
+	// Portable helpers
+	static int   Stricmp(const char* s1, const char* s2) { int d; while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1) { s1++; s2++; } return d; }
+	static int   Strnicmp(const char* s1, const char* s2, int n) { int d = 0; while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1) { s1++; s2++; n--; } return d; }
+	static char* Strdup(const char* s) { size_t len = strlen(s) + 1; void* buf = malloc(len); IM_ASSERT(buf); return (char*)memcpy(buf, (const void*)s, len); }
+	static void  Strtrim(char* s) { char* str_end = s + strlen(s); while (str_end > s && str_end[-1] == ' ') str_end--; *str_end = 0; }
+
+	void ClearLog();
+	void AddLog(const char* fmt, ...) IM_FMTARGS(2);
+	void DrawConsole(const char* title);
+	void ExecCommand(const char* command_line);
+	static int TextEditCallbackStub(ImGuiInputTextCallbackData* data);
+	int TextEditCallback(ImGuiInputTextCallbackData* data);
+
 private:
 	ImGuiHandler();
 	~ImGuiHandler();
 
 	std::map<std::string, ImVec4> m_Vec4Map;
 	std::map<std::string, float> m_FloatMap;
+	std::map<std::string, char*> m_TextMap;
+
+	char m_InputBuf[256];
+	ImVector<char*> m_Items;
+	ImVector<const char*> m_Commands;
+	ImVector<char*> m_History;
+	int m_HistoryPos;
+	ImGuiTextFilter m_Filter;
+	bool m_ScrollToBottom;
 };
 
 #endif
