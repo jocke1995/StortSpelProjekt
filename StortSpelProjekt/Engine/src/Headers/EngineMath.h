29e919ab93fb226b7680a233463e83c97d7d9f44
Tim Johansson <totimom@live.se>
2020-09-30 13:36:51 +0200

    Added doubles to vector definitions.

--- StortSpelProjekt/Engine/src/Headers/EngineMath.h
+++ StortSpelProjekt/Engine/src/Headers/EngineMath.h
@@ -23,4 +23,22 @@ typedef union
 	struct { float u; float v; };
 } float2;
 
+typedef union
+{
+	struct { double x; double y; double z; double w; };
+	struct { double r; double g; double b; double a; };
+} double4;
+
+typedef union
+{
+	struct { double x; double y; double z; };
+	struct { double r; double g; double b; };
+} double3;
+
+typedef union
+{
+	struct { double x; double y; };
+	struct { double u; double v; };
+} double2;
+
 #endif
