f4cdfe7799a9fecf86067439bd2a561b5c074798
Leo Wikström <wikstrom.hl@gmail.com>
2020-10-15 14:04:33 +0200

    Fixed player mass

--- StortSpelProjekt/Sandbox/src/main.cpp
+++ StortSpelProjekt/Sandbox/src/main.cpp
@@ -317,7 +317,7 @@ Scene* LeosTestScene(SceneManager* sm)
 
     double rad = playerDim.z / 2.0;
     double cylHeight = playerDim.y - (rad * 2.0);
-    ccc = entity->AddComponent<component::CapsuleCollisionComponent>(20000.0, rad, cylHeight, 0.0, 0.0, false);
+    ccc = entity->AddComponent<component::CapsuleCollisionComponent>(200.0, rad, cylHeight, 0.0, 0.0, false);
     hc = entity->AddComponent<component::HealthComponent>(50);
     ic->Init();
     bbc->Init();
