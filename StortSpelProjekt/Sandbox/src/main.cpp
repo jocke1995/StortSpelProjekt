928398c685ea76d8dd54478eb7fb323cf48d3432
Joakim Sjöberg <joakim.sjo@gmail.com>
2020-10-27 10:59:50 +0100

    Merge issues

--- StortSpelProjekt/Sandbox/src/main.cpp
+++ StortSpelProjekt/Sandbox/src/main.cpp
@@ -55,8 +55,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 
     //Scene* jacobScene = JacobsTestScene(sceneManager);
     //Scene* activeScenes[] = { jacobScene };
-    Scene* leoScene = LeosTestScene(sceneManager);
-    Scene* activeScenes[] = { leoScene };
+    //Scene* leoScene = LeosTestScene(sceneManager);
+    //Scene* activeScenes[] = { leoScene };
     //Scene* timScene = TimScene(sceneManager);
     //Scene* activeScenes[] = { timScene };
     //Scene* jockeScene = JockesTestScene(sceneManager);
