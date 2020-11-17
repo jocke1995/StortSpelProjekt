21978eff363cde0df319be176044fd4f16ade112
Anton Å <anton.asbrink@hotmail.com>
2020-11-17 11:00:45 +0100

    Particles

--- StortSpelProjekt/Game/src/Gamefiles/Components/PoisonDebuff.cpp
+++ StortSpelProjekt/Game/src/Gamefiles/Components/PoisonDebuff.cpp
@@ -12,23 +12,22 @@
 component::PoisonDebuff::PoisonDebuff(Entity* parent, int damagePerTick, int ticks, double tickDuration, float percentageSlow) : Component(parent)
 {
 	ParticleEffectSettings settings = {};
-	settings.particleCount = 500;
-	settings.startValues.lifetime = 0.8;
+	settings.particleCount = 50;
+	settings.startValues.lifetime = 0.5;
 	settings.spawnInterval = settings.startValues.lifetime / settings.particleCount;
-	settings.startValues.acceleration = { 0, -3, 0 };
+	settings.startValues.acceleration = { 0, -2, 0 };
 
 	// Need to fix EngineRand.rand() for negative values
-	RandomParameter3 randParam1 = { -2, 2, -2, 2, -2, 2 };
-	randParam1.y = { 2, 6 };
+	RandomParameter3 randParam1 = { -2, 2, 1, 4, -2, 2 };
 
-	settings.randPosition = { 0, 1, 0, 1, 0, 1 };
+	settings.randPosition = { -1, 1, -1, 1, -1, 1 };
 	settings.randVelocity = randParam1;
-	settings.randSize = { 0.2, 2 };
+	settings.randSize = { 0.2, 1 };
 	settings.randRotationSpeed = { 0, 3 };
 
 	Texture2DGUI* particleTexture = static_cast<Texture2DGUI*>(AssetLoader::Get()->LoadTexture2D(L"../Vendor/Resources/Textures/Particles/poison_particle.png"));
 	parent->AddComponent<component::ParticleEmitterComponent>(particleTexture, &settings, true);
-	parent->GetComponent<component::ParticleEmitterComponent>()->Play();
+	parent->GetComponent<component::ParticleEmitterComponent>()->OnInitScene();
 	Reset(damagePerTick, ticks, tickDuration, percentageSlow);
 }
 
@@ -53,6 +52,7 @@ void component::PoisonDebuff::Reset(int damagePerTick, int ticks, double tickDur
 	m_Slow = transform->GetVelocity() * percentageSlow;
 	transform->SetVelocity(transform->GetVelocity() - m_Slow);
 	m_TickTimer = 0.0;
+	m_pParent->GetComponent<component::ParticleEmitterComponent>()->Play();
 }
 
 void component::PoisonDebuff::Update(double dt)
@@ -69,6 +69,7 @@ void component::PoisonDebuff::Update(double dt)
 			{
 				Transform* transform = m_pParent->GetComponent<component::TransformComponent>()->GetTransform();
 				transform->SetVelocity(transform->GetVelocity() + m_Slow);
+				m_pParent->GetComponent<component::ParticleEmitterComponent>()->Stop();
 			}
 		}
 	}
