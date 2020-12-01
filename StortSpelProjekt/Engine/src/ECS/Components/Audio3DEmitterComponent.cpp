07b201af4bdc1ac38c270f1623c325accb5e63c8
Tim Johansson <totimom@live.se>
2020-12-01 16:03:34 +0100

    If the sound does not exist, the emitter should not be updated.

--- StortSpelProjekt/Engine/src/ECS/Components/Audio3DEmitterComponent.cpp
+++ StortSpelProjekt/Engine/src/ECS/Components/Audio3DEmitterComponent.cpp
@@ -33,36 +33,39 @@ void component::Audio3DEmitterComponent::OnUnInitScene()
 
 void component::Audio3DEmitterComponent::UpdateEmitter(const std::wstring& name)
 {
-	// get parent entity and look for transform components and get update position
-	m_pTransform = m_pParent->GetComponent<TransformComponent>()->GetTransform();
-	m_VoiceEmitterData[name].emitter.Position = m_pTransform->GetPositionXMFLOAT3();
-	// update front and top vectors
-	DirectX::XMMATRIX rotMat = m_pTransform->GetRotMatrix();
-	DirectX::XMFLOAT3 forward, up;
-	DirectX::XMStoreFloat3(&forward, rotMat.r[2]);
-	DirectX::XMStoreFloat3(&up, rotMat.r[1]);
-	m_VoiceEmitterData[name].emitter.OrientFront = forward;
-	m_VoiceEmitterData[name].emitter.OrientTop = up;
+	if (m_VoiceEmitterData.count(name) != 0)
+	{
+		// get parent entity and look for transform components and get update position
+		m_pTransform = m_pParent->GetComponent<TransformComponent>()->GetTransform();
+		m_VoiceEmitterData[name].emitter.Position = m_pTransform->GetPositionXMFLOAT3();
+		// update front and top vectors
+		DirectX::XMMATRIX rotMat = m_pTransform->GetRotMatrix();
+		DirectX::XMFLOAT3 forward, up;
+		DirectX::XMStoreFloat3(&forward, rotMat.r[2]);
+		DirectX::XMStoreFloat3(&up, rotMat.r[1]);
+		m_VoiceEmitterData[name].emitter.OrientFront = forward;
+		m_VoiceEmitterData[name].emitter.OrientTop = up;
 
-	// calculates values relative to the emitter and listener and saves values in emitter.DSPSettings
-	X3DAudioCalculate(
-		*AudioEngine::GetInstance().GetX3DInstance(),
-		AudioEngine::GetInstance().GetListener(),
-		&m_VoiceEmitterData[name].emitter,
-		X3DAUDIO_CALCULATE_MATRIX,
-		&m_VoiceEmitterData[name].DSPSettings);
+		// calculates values relative to the emitter and listener and saves values in emitter.DSPSettings
+		X3DAudioCalculate(
+			*AudioEngine::GetInstance().GetX3DInstance(),
+			AudioEngine::GetInstance().GetListener(),
+			&m_VoiceEmitterData[name].emitter,
+			X3DAUDIO_CALCULATE_MATRIX,
+			&m_VoiceEmitterData[name].DSPSettings);
 
-	// temporary solution due to strange behaviour of the way matrixcoefficients are saved
-	float temp = m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[1];
-	m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[1] = m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[2];
-	m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[2] = temp;
+		// temporary solution due to strange behaviour of the way matrixcoefficients are saved
+		float temp = m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[1];
+		m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[1] = m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[2];
+		m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients[2] = temp;
 
-	// sets output matrix for the sound according to the matrix coefficients calculated earlier (changes the soundchannel levels)
-	m_VoiceEmitterData[name].voice.GetSourceVoice()->SetOutputMatrix(
-		AudioEngine::GetInstance().GetMasterVoice(),
-		m_VoiceEmitterData[name].voiceDetails.InputChannels,
-		AudioEngine::GetInstance().GetDeviceDetails()->InputChannels,
-		m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients);
+		// sets output matrix for the sound according to the matrix coefficients calculated earlier (changes the soundchannel levels)
+		m_VoiceEmitterData[name].voice.GetSourceVoice()->SetOutputMatrix(
+			AudioEngine::GetInstance().GetMasterVoice(),
+			m_VoiceEmitterData[name].voiceDetails.InputChannels,
+			AudioEngine::GetInstance().GetDeviceDetails()->InputChannels,
+			m_VoiceEmitterData[name].DSPSettings.pMatrixCoefficients);
+	}
 }
 
 void component::Audio3DEmitterComponent::AddVoice(const std::wstring& name)
