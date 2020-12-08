152e261496bf083616d0a5123437311c388ba704
William <ville.or@hotmail.com>
2020-12-08 15:14:44 +0100

    Minor code clarification

--- StortSpelProjekt/Game/src/Gamefiles/Components/PlayerInputComponent.h
+++ StortSpelProjekt/Game/src/Gamefiles/Components/PlayerInputComponent.h
@@ -79,7 +79,7 @@ namespace component
 		double m_DashTimer;
 		bool m_DashReady;
 		bool m_Dashing;
-		unsigned char m_IsMoving = false;
+		unsigned char m_WasMoving = false;
 		bool m_MovementStateChanged = false;
 
 		// Is used to determine if the player is attacking, and should be turned in the camera direction, or if she should turn in the direction she is moving
