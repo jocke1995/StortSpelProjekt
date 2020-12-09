c4322d7af0e5a43551633bf4ca2451472d4fcce8
Björn Johansson <andromeda_stargate@hotmail.com>
2020-12-09 11:21:08 +0100

    Fixed the pricing since I forgot that m_Level starts at 0
    Also added comment at the top of .cpp file which describes the pricing and level increase for the upgrade.

--- StortSpelProjekt/Game/src/Gamefiles/Components/UpgradeComponents/Upgrades/UpgradeMeleeLifeSteal.cpp
+++ StortSpelProjekt/Game/src/Gamefiles/Components/UpgradeComponents/Upgrades/UpgradeMeleeLifeSteal.cpp
@@ -5,6 +5,10 @@
 #include "UpgradeMeleeLifeSteal.h"
 #include "Components/MeleeComponent.h"
 
+// Gives 10% lifesteal to melee attacks. Subsequent levels will add 5% more lifesteal. 
+// This procentage is based on the melee damage you do so 50 meleeDamage will give 5hp in lifesteal.
+// Price scaling should be 300 -> 700 -> 1300 -> 2100 -> 3100 -> 4300 -> 5700 -> 7300 -> 9100 etc.
+// Lifesteal at these lvls 10% -> 15% -> 20%  -> 25%  -> 30%  -> 35%  -> 40%  -> 45%  -> 50%  etc.
 UpgradeMeleeLifeSteal::UpgradeMeleeLifeSteal(Entity* parent) : Upgrade(parent)
 {
 	// name the upgrade!
@@ -29,7 +33,7 @@ void UpgradeMeleeLifeSteal::IncreaseLevel()
 {
 	m_Level++;
 	m_PercentageGain = 0.05f + 0.05f * m_Level; // Starts at 10% and increases by 5% per level
-	m_Price += m_Level * 200;
+	m_Price += (m_Level + 1) * 200; // m_Level starts at 0 so we increase it by 1.
 }
 
 void UpgradeMeleeLifeSteal::OnMeleeHit(Entity* target)
