800adcbbb6ef7e45d1ddd5fff11766297e6fd2fe
Tim Johansson <totimom@live.se>
2020-12-10 17:06:04 +0100

    memLeakFix
    Turns out an early return caused all this headache.

--- StortSpelProjekt/Engine/src/Misc/Window.cpp
+++ StortSpelProjekt/Engine/src/Misc/Window.cpp
@@ -81,6 +81,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		LPBYTE lpb = new BYTE[dwSize];
 		if (lpb == NULL)
 		{
+			delete[] lpb;
 			return 0;
 		}
 
@@ -101,19 +102,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			if ( key == SCAN_CODES::END || (key == SCAN_CODES::F4 && Input::GetInstance().GetKeyState(SCAN_CODES::ALT)))
 			{
 				// Check if not pressed up (case where HOLD f4 -> HOLD alt -> RELEASE f4)
-				if (inputData.Flags == RI_KEY_BREAK)
+				if (inputData.Flags != RI_KEY_BREAK)
 				{
-					return 0;
+					// UnSet alt
+					Input::GetInstance().SetKeyState(SCAN_CODES::ALT, false);
+
+					// Quit
+					programRunning = false;
+					DestroyWindow(hWnd);
 				}
-
-				// UnSet alt
-				Input::GetInstance().SetKeyState(SCAN_CODES::ALT, false);
-
-				// Quit
-				programRunning = false;
-				DestroyWindow(hWnd);
-
-				return 0;
 			}
 
 			Input::GetInstance().SetKeyState(key, !(inputData.Flags % 2));
